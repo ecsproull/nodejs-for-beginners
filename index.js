a805c30bb68bf66145e1c31c664ce1087c5ac46a
ecsproull <ecs3@po.cwru.edu>
2025-02-17 13:32:38 -0700

    Style fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { app } from './server.js'
 import mongoose from 'mongoose'
 
 const port = process.env.PORT
-try{
+try {
   await mongoose.connect(process.env.MONGODB_URI)
   console.log('Connected to MongoDB')
   app.listen(port, () => {
