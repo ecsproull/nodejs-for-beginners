2cda803b9fc9a8ee8ef68c7fb745b4ac750539db
ecsproull <ecs3@po.cwru.edu>
2025-02-17 11:08:03 -0700

    Harded coded connection string, again

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose'
 const port = process.env.PORT
 
 try {
-  await mongoose.connect(process.env.MONGODB_URI)
+  await mongoose.connect("mongodb+srv://ecsproull765:9wMNPgo4IofE1HWM@nodejs-for-beginners.1ss5p.mongodb.net/")
   console.log('Connected to MongoDB')
   app.listen(port, () => {
     console.log(`Running in http://localhost:${port}`)
